2755ad5551ba980e5d870fd6aa7764931a60a763
jessacabigas <jessacabigas@gmail.com>
2016-12-17 14:42:45 +0800

    Secondary commit.

---
+++ readme.txt~
@@ -0,0 +1,12 @@
+FIFO:
+
+Made of use of an ArrayList for the frames. Each iteration will be displayed with an indication of "H" if it is a hit or "F" if a page fault occurs. A variable of which is index to be replaced is updated at each page inserted into the frame.
+
+LRU:
+
+Made use of an array to keep track of which index was recently used. If a page fault occurs, the mininum value among the count array will check which index has been least recently used. Each iteration will be displayed with an indication of "H" if it is a hit or "F" if a page fault occurs.
+
+LFU:
+
+Made use of an array to store the frequency count of each index in the frames. The index with least amount of frequency will be replaced when a page fault occurs. Each iteration will be displayed with an indication of "H" if it is a hit or "F" if a page fault occurs.
+
--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,12 @@ import java.util.Scanner;
  */
 public class Main {
 
+    ArrayList frames;
+    int[] ref;
+
+    @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
+        Main menu = new Main();
         Scanner scan = new Scanner(System.in);
         System.out.println("Menu:\n1.FIFO\n2.LFU\n3.LRU");
         System.out.println("Choice: ");
@@ -29,41 +34,152 @@ public class Main {
         System.out.print("Length of reference:");
 
         int length = scan.nextInt();
-        int[] ref = new int[length];
+        menu.ref = new int[length];
+
         for (int i = 0; i < length; i++) {
             System.out.print("Reference [" + (i + 1) + "]: ");
-            ref[i] = scan.nextInt();
+            menu.ref[i] = scan.nextInt();
         }
-
-        System.out.println(Arrays.toString(ref));
+        System.out.println(Arrays.toString(menu.ref));
         System.out.print("Page frame(s): ");
-        int frames = scan.nextInt();
-        if (choice == 1) {
-            FIFO(ref, frames);
+
+        int size = scan.nextInt();
+        switch (choice) {
+            case 1:
+                menu.FIFO(menu.ref, size);
+                break;
+            case 3:
+                menu.LRU(menu.ref, size);
+                break;
+            case 2:
+                menu.LFU(menu.ref, size);
+                break;
+            default:
+                break;
         }
 
     }
 
-    private static void FIFO(int[] ref, int size) {
+    private void FIFO(int[] ref, int size) {
         int replace = 0;
         int faults = 0;
-        ArrayList table = new ArrayList();
+        frames = new ArrayList();
         for (int i = 0; i < ref.length; i++) {
-            if (table.contains(ref[i])) {
-                System.out.println("H: " + table.toString());
-            } else if (table.size() < size) {
-                table.add(ref[i]);
-                System.out.println("F: " + table.toString());
+            if (frames.contains(ref[i])) {
+                System.out.println("H: " + frames.toString());
+            } else if (frames.size() < size) {
+                frames.add(ref[i]);
+                System.out.println("F: " + frames.toString());
                 faults++;
             } else {
-                table.set(replace, ref[i]);
-                System.out.println("F: " + table.toString());
+                frames.set(replace, ref[i]);
+                System.out.println("F: " + frames.toString());
                 replace++;
                 faults++;
             }
             if (replace == size) {
                 replace = 0;
             }
+        }
+        System.out.println("Page Faults: " + faults);
+    }
+
+    private void LRU(int[] ref, int size) {
+        int faults = 0;
+        int replace = 0;
+        int[] count = new int[size];
+        frames = new ArrayList();
+        int j = 0;
+        for (int i = 0; i < ref.length; i++) {
+            if (frames.contains(ref[i])) {
+                System.out.println("H: " + frames.toString());
+                count[frames.indexOf(ref[i])] = i + 1;
+
+            } else if (frames.size() < size) {
+                frames.add(ref[i]);
+                System.out.println("F: " + frames.toString());
+                count[j] = i;
+                faults++;
+            } else {
+                replace = getMin(count);
+                frames.set(replace, ref[i]);
+                System.out.println("F: " + frames.toString());
+                count[replace] = i;
+                faults++;
+            }
+
+            if (j >= size) {
+                j = 0;
+            }
+            j++;
+        }
+        System.out.println("Page Faults: " + faults);
+    }
+
+    private int getMin(int[] count) {
+        int min = 0;
+        //stable ni sha si FIFO jpun
+        for (int i = 0; i < count.length; i++) {
+            if (count[min] > count[i]) {
+                min = i;
+            }
+        }
+        return min;
+    }
+
+    private void LFU(int[] ref, int size) {
+        int f, p, num = 0, min = 0;
+        int faults = 0;
+        frames = new ArrayList();
+        int freq[] = new int[3];
+        int count[] = new int[10];
+        boolean flag = true;
+        for (int i = 0; i < size; i++) {
+            frames.add(" ");
+            freq[i] = -1;
+        }
+
+        for (int i = 0; i < ref.length; i++) {
+            flag = true;
+            int page = ref[i];
+
+            for (int j = 0; j < size; j++) {
+                if (frames.get(j).equals(page)) {
+                    flag = false;
+                    count[page]++;
+                    System.out.println("H:" + frames.toString());
+                    break;
+                }
+            }
+            if (flag) {
+                if (i >= 3) {
+                    for (int j = 0; j < size; j++) {
+                        num = (int) frames.get(j);
+                        freq[j] = count[num];
+                    }
+                    min = freq[0];
+                    for (int j = 0; j < size; j++) {
+                        if (freq[j] < min) {
+                            min = freq[j];
+                        }
+                    }
+                    for (int j = 0; j < size; j++) {
+                        if (freq[j] == min) {
+                            count[page]++;
+                            frames.set(j, page);
+                            System.out.println("F:" + frames.toString());
+                            faults++;
+                            break;
+                        }
+                    }
+                } else {
+                    frames.set(i, page);
+                    count[page]++;
+                    System.out.println("F:" + frames.toString());
+                    faults++;
+                }
+            }
+
         }
         System.out.println("Page Faults: " + faults);
     }
